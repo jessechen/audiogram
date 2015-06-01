4da6b4a7bc1c8afa71aad5c66d198861888c5cb4
Ken Pratt <ken@kenpratt.net>
2015-06-01 17:03:04 -0400

    Send bits via pulses of a 19100Hz sine wave.

--- generate.rb
+++ generate.rb
@@ -1,42 +1,23 @@
 require "coreaudio"
 
-def send_calibration(buf)
-  frequency = 220.0
-  phase = Math::PI * 2.0 * frequency / 44100
+dev = CoreAudio.default_output_device
+buf = dev.output_buffer(1024)
 
+frequency = 19100.0
+phase = Math::PI * 2.0 * frequency / dev.nominal_rate
+thread = Thread.start do
+  data = [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1 ,1]
   i = 0
   wav = NArray.sint(1024)
-  loop do
-    1024.times do |j|
-      polarity = Math.sin(phase*(i+j))
-      wav[j] = polarity < 0 ? -0x7FFF : 0x7FFF
-    end
+  data.each do |bit|
+    1024.times {|j| wav[j] = (0.4 * Math.sin(phase*bit*(i+j)) * 0x7FFF).round }
     i += 1024
-    puts wav.inspect
     buf << wav
   end
 end
 
-dev = CoreAudio.default_output_device
-buf = dev.output_buffer(1024)
-
-frequency = 440.0
-phase = Math::PI * 2.0 * frequency / dev.nominal_rate
-thread = Thread.start do
-  puts "here"
-  send_calibration(buf)
-
-  i = 0
-  wav = NArray.sint(1024)
-  loop do
-    1024.times {|j| wav[j] = (0.4 * Math.sin(phase*(i+j)) * 0x7FFF).round }
-    i += 1024
-    # buf << wav
-  end
-end
-
 buf.start
-sleep 2
+sleep 0.5
 buf.stop
 
 thread.kill.join
--- receive.rb
+++ receive.rb
@@ -6,12 +6,15 @@ buf = dev.input_buffer(1024)
 thread = Thread.start do
   loop do
     waveform = buf.read(1024)
-    puts waveform[0, true].inspect
+    channel = waveform[0, true].to_a
+    channel.each_slice(16) do |chunk|
+      puts chunk.inspect
+    end
   end
 end
 
 buf.start
-sleep 10
+sleep 0.5
 buf.stop
 
 thread.kill.join
