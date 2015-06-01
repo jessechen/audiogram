cc8854599d1669eedeeb0a1de03340b5d4fc54ce
Jesse Chen <jesse.z.chen@gmail.com>
2015-06-01 15:02:27 -0400

    Square wave

--- feedback.rb
+++ feedback.rb
@@ -1,5 +1,22 @@
 require "coreaudio"
 
+def send_calibration(out_buf)
+  frequency = 220.0
+  phase = Math::PI * 2.0 * frequency / 44100
+
+  i = 0
+  wav = NArray.sint(1024)
+  loop do
+    1024.times do |j|
+      polarity = Math.sin(phase*(i+j))
+      wav[j] = polarity < 0 ? -0x7FFF : 0x7FFF
+    end
+    i += 1024
+    puts wav.inspect
+    out_buf << wav
+  end
+end
+
 in_dev = CoreAudio.default_input_device
 out_dev = CoreAudio.default_output_device
 in_buf = in_dev.input_buffer(1024)
@@ -8,12 +25,15 @@ out_buf = out_dev.output_buffer(1024)
 frequency = 440.0
 phase = Math::PI * 2.0 * frequency / out_dev.nominal_rate
 output_thread = Thread.start do
+  puts "here"
+  send_calibration(out_buf)
+
   i = 0
   wav = NArray.sint(1024)
   loop do
     1024.times {|j| wav[j] = (0.4 * Math.sin(phase*(i+j)) * 0x7FFF).round }
     i += 1024
-    out_buf << wav
+    # out_buf << wav
   end
 end
 
@@ -32,3 +52,5 @@ out_buf.stop
 
 output_thread.kill.join
 input_thread.kill.join
+
+
