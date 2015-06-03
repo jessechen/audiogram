026464488d6bc7403f480ee7597eaede6573d9a4
Ken Pratt <ken@kenpratt.net>
2015-06-03 00:14:39 -0400

    Fixed case where there weren't any occurrences.

--- receive.rb
+++ receive.rb
@@ -13,6 +13,7 @@ buf = CoreAudio.default_input_device.input_buffer(BUFFER_SIZE)
 FRACTIONAL_BIT_STREAM = Queue.new
 
 def highest_signal(occurrences)
+  return 0 unless occurrences.any?
   occurrences.max_by {|_, v| v}.first
 end
 
