8fd48f6ae5fd3bfe020182cd1436ab0e9cd02d30
Jesse Chen <jesse.z.chen@gmail.com>
2015-06-04 11:36:20 -0400

    Translate bits into Morse signals

--- telegraph.rb
+++ telegraph.rb
@@ -2,52 +2,60 @@ DOT = "·"
 DASH = "–"
 
 MORSE = {
-    [DOT,  DASH]                   => 'A',
-    [DASH, DOT,  DOT,  DOT ]       => 'B',
-    [DASH, DOT,  DASH, DOT ]       => 'C',
-    [DASH, DOT,  DOT ]             => 'D',
-    [DOT ]                         => 'E',
-    [DOT,  DOT,  DASH, DOT ]       => 'F',
-    [DASH, DASH, DOT ]             => 'G',
-    [DOT,  DOT,  DOT,  DOT ]       => 'H',
-    [DOT,  DOT ]                   => 'I',
-    [DOT,  DASH, DASH, DASH]       => 'J',
-    [DASH, DOT,  DASH]             => 'K',
-    [DOT,  DASH, DOT,  DOT ]       => 'L',
-    [DASH, DASH]                   => 'M',
-    [DASH, DOT ]                   => 'N',
-    [DASH, DASH, DASH]             => 'O',
-    [DOT,  DASH, DASH, DOT ]       => 'P',
-    [DASH, DASH, DOT,  DASH]       => 'Q',
-    [DOT,  DASH, DOT ]             => 'R',
-    [DOT,  DOT,  DOT ]             => 'S',
-    [DASH]                         => 'T',
-    [DOT,  DOT,  DASH]             => 'U',
-    [DOT,  DOT,  DOT,  DASH]       => 'V',
-    [DOT,  DASH, DASH]             => 'W',
-    [DASH, DOT,  DOT,  DASH]       => 'X',
-    [DASH, DOT,  DASH, DASH]       => 'Y',
-    [DASH, DASH, DOT,  DOT ]       => 'Z',
-    [DASH, DASH, DASH, DASH, DASH] => '0',
-    [DOT,  DASH, DASH, DASH, DASH] => '1',
-    [DOT,  DOT,  DASH, DASH, DASH] => '2',
-    [DOT,  DOT,  DOT,  DASH, DASH] => '3',
-    [DOT,  DOT,  DOT,  DOT,  DASH] => '4',
-    [DOT,  DOT,  DOT,  DOT,  DOT ] => '5',
-    [DASH, DOT,  DOT,  DOT,  DOT ] => '6',
-    [DASH, DASH, DOT,  DOT,  DOT ] => '7',
-    [DASH, DASH, DASH, DOT,  DOT ] => '8',
-    [DASH, DASH, DASH, DASH, DOT ] => '9',
+  [DOT,  DASH]                   => 'A',
+  [DASH, DOT,  DOT,  DOT ]       => 'B',
+  [DASH, DOT,  DASH, DOT ]       => 'C',
+  [DASH, DOT,  DOT ]             => 'D',
+  [DOT ]                         => 'E',
+  [DOT,  DOT,  DASH, DOT ]       => 'F',
+  [DASH, DASH, DOT ]             => 'G',
+  [DOT,  DOT,  DOT,  DOT ]       => 'H',
+  [DOT,  DOT ]                   => 'I',
+  [DOT,  DASH, DASH, DASH]       => 'J',
+  [DASH, DOT,  DASH]             => 'K',
+  [DOT,  DASH, DOT,  DOT ]       => 'L',
+  [DASH, DASH]                   => 'M',
+  [DASH, DOT ]                   => 'N',
+  [DASH, DASH, DASH]             => 'O',
+  [DOT,  DASH, DASH, DOT ]       => 'P',
+  [DASH, DASH, DOT,  DASH]       => 'Q',
+  [DOT,  DASH, DOT ]             => 'R',
+  [DOT,  DOT,  DOT ]             => 'S',
+  [DASH]                         => 'T',
+  [DOT,  DOT,  DASH]             => 'U',
+  [DOT,  DOT,  DOT,  DASH]       => 'V',
+  [DOT,  DASH, DASH]             => 'W',
+  [DASH, DOT,  DOT,  DASH]       => 'X',
+  [DASH, DOT,  DASH, DASH]       => 'Y',
+  [DASH, DASH, DOT,  DOT ]       => 'Z',
+  [DASH, DASH, DASH, DASH, DASH] => '0',
+  [DOT,  DASH, DASH, DASH, DASH] => '1',
+  [DOT,  DOT,  DASH, DASH, DASH] => '2',
+  [DOT,  DOT,  DOT,  DASH, DASH] => '3',
+  [DOT,  DOT,  DOT,  DOT,  DASH] => '4',
+  [DOT,  DOT,  DOT,  DOT,  DOT ] => '5',
+  [DASH, DOT,  DOT,  DOT,  DOT ] => '6',
+  [DASH, DASH, DOT,  DOT,  DOT ] => '7',
+  [DASH, DASH, DASH, DOT,  DOT ] => '8',
+  [DASH, DASH, DASH, DASH, DOT ] => '9',
 }
 
 def decode(morse_signals)
   MORSE[morse_signals] || '?'
 end
 
-def bits_to_signals(bit_fractions)
+def bits_to_signals(bits)
   # dot = 1x1
   # dash = 1x3
   # end_of_signal = 0x1
   # end_of_char = 0x3
   # end_of_word = 0x7
+
+  bit_string = bits.join
+  bit_string = bit_string.gsub(/0{3,}/, ' ')  # 3 or more consecutive zeroes
+  bit_string = bit_string.gsub(/1{3,}/, DASH) # 3 or more consecutive ones
+  bit_string = bit_string.gsub(/1+/, DOT)
+  bit_string = bit_string.gsub(/0+/, '')
+  bit_string.split(' ').map {|s| s.split('')}
 end
+
--- telegraph_spec.rb
+++ telegraph_spec.rb
@@ -1,7 +1,29 @@
 require './telegraph'
 
 describe 'telegraph' do
-  it 'should run tests' do
-    expect(2+2).to eq(4)
+  it 'should decode morse signal arrays' do
+    expect(decode([DOT])).to eq('E')
+    expect(decode([DOT, DOT, DOT])).to eq('S')
+    expect(decode([DASH, DOT, DOT, DOT, DOT])).to eq('6')
+    expect(decode([DOT, DOT, DASH, DASH])).to eq('?')
+  end
+
+  it 'should convert bits into morse signal arrays' do
+    expect(bits_to_signals([1])).to eq([[DOT]])
+    expect(bits_to_signals([1, 1, 1])).to eq([[DASH]])
+    expect(bits_to_signals([1, 0, 1])).to eq([[DOT, DOT]])
+    expect(bits_to_signals([1, 0, 1, 1, 1])).to eq([[DOT, DASH]])
+  end
+
+  it 'should split words on runs of three zeros' do
+    expect(bits_to_signals([1, 0, 0, 0, 1])).to eq([[DOT], [DOT]])
+    expect(bits_to_signals([1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1])).to eq([[DASH, DOT], [DASH], [DOT]])
+  end
+
+  it 'should treat runs of two ones as dots and runs of four or more ones as dashes' do
+    expect(bits_to_signals([1, 1])).to eq([[DOT]])
+    expect(bits_to_signals([1, 1, 1, 1])).to eq([[DASH]])
+    expect(bits_to_signals([1, 1, 1, 1, 1, 0, 0, 1, 1])).to eq([[DASH, DOT]])
+    expect(bits_to_signals([1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1])).to eq([[DASH], [DOT]])
   end
 end
